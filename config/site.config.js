02f4eed986ca2a5107528f9e9c0953c4535f3522
Romain Giovanetti <ganoninc@gmail.com>
2020-02-13 18:46:06 +0100

    fix broken URLs and add dark mode support

--- config/site.config.js
+++ config/site.config.js
@@ -18,7 +18,7 @@ const config = {
   site_url: 'https://giovanetti.fr/',
 
   // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: '',
+  googleAnalyticsUA: 'UA-110583701-1',
 
   // The viewport meta tag added to your HTML page's <head> tag
   viewport: 'width=device-width,initial-scale=1',
