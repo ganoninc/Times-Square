fa0b5e8f725a108c5bd916879efff24fad3fe588
Romain Giovanetti <ganoninc@gmail.com>
2022-10-14 13:54:56 +1100

    update GoogleAnalyticsPlugin to GA4

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -109,7 +109,16 @@ const webpackBar = new WebpackBar({
 });
 
 // Google analytics
-const CODE = `<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','{{ID}}','auto');ga('send','pageview');</script>`;
+const CODE = `<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ID}}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '{{ID}}');
+</script>
+`;
 
 class GoogleAnalyticsPlugin {
   constructor({ id }) {
@@ -121,7 +130,7 @@ class GoogleAnalyticsPlugin {
       HTMLWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
         'GoogleAnalyticsPlugin',
         (data, cb) => {
-          data.html = data.html.replace('</head>', `${CODE.replace('{{ID}}', this.id) }</head>`);
+          data.html = data.html.replace('</head>', `${CODE.replace(/{{ID}}/g, this.id) }</head>`);
           cb(null, data);
         },
       );
